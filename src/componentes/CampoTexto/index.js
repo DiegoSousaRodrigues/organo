4eccb0a4d09dfbe882329e5a675bcec10c057912
DiegoSousa1 <diego.rodigues@cws.digital>
2023-03-19 20:46:06 -0300

    aprendendo sobre estado no react

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -3,10 +3,19 @@ import "./CampoTexto.css";
 const CampoTexto = (props) => {
   const placeholderModificada = `${props.placeholder.toLowerCase()}...`;
 
+  let valor = 'Diego Sousa'
+
+  const aoDigitado = (evento) => {
+    valor = evento.target.value;
+    console.log(valor);
+  }
+
   return (
     <div className="campo-texto">
       <label> {props.label} </label>
       <input
+        value={valor}
+        onChange={aoDigitado}
         required={props.obrigatorio}
         placeholder={"Digite o seu " + placeholderModificada}
       />
